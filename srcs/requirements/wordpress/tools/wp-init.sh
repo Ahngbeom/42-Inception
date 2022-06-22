7f12a5609d83defe295cc5c9c59fcac7f550479f
ahngbeom <bbu0704@gmail.com>
2022-06-22 23:24:17 +0900

    Use Environment Value for wp-config.php

--- srcs/requirements/wordpress/tools/wp-init.sh
+++ srcs/requirements/wordpress/tools/wp-init.sh
@@ -17,12 +17,20 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	# Korean Language WordPress
 	cd /var/www/html && wp core download --allow-root --locale=ko_KR ;
 
-	mv /var/temp/wp-config.php /var/www/html/
+	# mv /var/temp/wp-config.php /var/www/html/
 	
+	# wp config create --allow-root --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST};
+	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+	sed -i "s/username_here/$WORDPRESS_DB_USER/g" /var/www/html/wp-config.php
+	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" /var/www/html/wp-config.php
+	sed -i "s/localhost/$WORDPRESS_DB_HOST/g" /var/www/html/wp-config.php
+	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/g" /var/www/html/wp-config.php
+
 	# wp core install & set admin
 	wp core install --allow-root --url=${DOMAIN_NAME} --title=Inception --admin_user=${WORDPRESS_DB_ADMIN} --admin_password=${WORDPRESS_DB_ADMIN_PASSWORD} --admin_email=${WORDPRESS_DB_ADMIN_EMAIL}
+	
 	# wp user create & set user
-	wp user create --allow-root ${WORDPRESS_DB_USER} ${WORDPRESS_DB_EMAIL} --user_pass=${WORDPRESS_DB_PASSWORD};
+	wp user create --allow-root ${WORDPRESS_DB_USER} ${WORDPRESS_DB_EMAIL} --user_pass=${WORDPRESS_DB_PASSWORD}
 
 fi
 
