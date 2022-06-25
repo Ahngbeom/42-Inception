fb9bd7cb6a736d22ccaf0067d1128736e4699386
ahngbeom <bbu0704@gmail.com>
2022-06-26 01:11:12 +0900

    Finding a solution of use environment value on mariadb configuration

--- srcs/requirements/mariadb/tools/db-init.sql
+++ srcs/requirements/mariadb/tools/db-init.sql
@@ -7,11 +7,11 @@ CREATE user IF NOT EXISTS 'bahn'@'%' identified BY '0704';
 GRANT all privileges ON wordpress.* TO 'bahn'@'%';
 -- CREATE user IF NOT EXISTS '$MARIADB_USER'@'%' identified BY '$MARIADB_PASSWORD';
 -- GRANT all privileges ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';
-flush privileges;
 
 -- 관리자 계정 생성
 CREATE user IF NOT EXISTS '42'@'%' identified BY '4242';
 GRANT all privileges ON wordpress.* TO '42'@'%';
 -- CREATE user IF NOT EXISTS '$MARIADB_ADMIN'@'%' identified BY '$MARIADB_ADMIN_PASSWORD';
 -- GRANT all privileges ON $MARIADB_DATABASE.* TO '$MARIADB_ADMIN'@'%';
+
 flush privileges;
