268c1214ab3168c90ea3815975e6979b7f842125
ahngbeom <bbu0704@gmail.com>
2022-06-28 23:34:51 +0900

    Delete wp-config.php & Add WordPress theme & KR language

--- srcs/requirements/wordpress/tools/wp-init.sh
+++ srcs/requirements/wordpress/tools/wp-init.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 set -e
 
-	### listen = /run/php/php7.3-fpm.sock 패턴을 listen = 9000 으로 변경
-	# sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
-	# chown -R www-data:www-data /var/www/*;
-	# chown -R 755 /var/www/*;
-	# mkdir -p /run/php/ && touch /run/php/php7.3-fpm.pid;
+# sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g" /etc/php/7.3/fpm/pool.d/www.conf
 
+### /var/www/html/wp-config.php이 존재하지않거나 정규 파일이 아닐 경우
 if [ ! -f /var/www/html/wp-config.php ]; then
 
 	### Check wp info
 	wp --info
 
-	### Korean Language WordPress
-	# cd /var/www/html && wp core download --allow-root --locale=ko_KR;
+	### Korean Language WordPress. 한글판 워드프레스 구성파일들을 현재 디렉토리에 다운로드
 	cd /var/www/html && wp core download --allow-root;
 
-	# wp config create --allow-root --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST};
+	### wp-config-sample.php 샘플 파일 복사
 	cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 	
 	### sed -i : 찾은 문자열 위에 다른 문자열로 대체
@@ -26,11 +22,22 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	sed -i "s/localhost/$MARIADB_HOST/g" /var/www/html/wp-config.php
 	sed -i "s/database_name_here/$MARIADB_DATABASE/g" /var/www/html/wp-config.php
 
-	### wp core install & set admin
-	# wp core install --allow-root --url=${DOMAIN_NAME} --title=Inception --admin_user=${WORDPRESS_DB_ADMIN} --admin_password=${WORDPRESS_DB_ADMIN_PASSWORD} --admin_email=${WORDPRESS_DB_ADMIN_EMAIL}
+	### wordpress core install & set admin
+	wp core install --allow-root --url=${DOMAIN_NAME} --title=Inception --admin_user=${MARIADB_ADMIN} --admin_password=${MARIADB_ADMIN_PASSWORD} --admin_email=${MARIADB_ADMIN_EMAIL}
 	
-	### wp user create & set user
-	# wp user create --allow-root ${WORDPRESS_DB_USER} ${WORDPRESS_DB_EMAIL} --user_pass=${WORDPRESS_DB_PASSWORD}
+	### wordpress user create & set user
+	wp user create --allow-root ${MARIADB_USER} ${MARIADB_EMAIL} --user_pass=${MARIADB_PASSWORD}
+
+	### get wordpress user list
+	wp user list --allow-root;
+
+	### wordpress theme install & activate
+	wp theme install twentytwenty --allow-root --activate;
+
+	### wordpress korean language install & activate
+	wp language core install --allow-root ko_KR;
+	### *** VM VirtualBox에서 구동시켰을 때, FireFox 브라우저에서 한글이 깨지는 현상이 발생한다.
+	# wp language core activate --allow-root ko_KR;
 
 fi
 
